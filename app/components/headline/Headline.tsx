9ce7272dca0377f161e93898eff94ecb9d11bd9c
Oséias Costa <114628443+oseias-costa@users.noreply.github.com>
2023-10-31 21:23:15 +0000

    feat(Headline): insert photo Juliana Paes

--- app/components/headline/Headline.tsx
+++ app/components/headline/Headline.tsx
@@ -1,39 +1,33 @@
 "use client";
 import * as S from "./headeline.styles";
-import Logo from '@/public/headline/logo-mastercred.svg'
-import { CldVideoPlayer } from 'next-cloudinary'
-import { useEffect } from "react";
-import ReactPlayer from "react-player/lazy";
-import Poster from './juliana-paes.jpg'
+import Logo from "@/public/headline/logo-mastercred.svg";
+import Juliana from "@/public/headline/juliana.png";
 
-export default function Headline(){
-
-    return(
-        <S.HeadlineContainer>
-           <S.HeadlineDescription>
-                <S.LogoIcon src={Logo} width={180} alt='' />
-                <S.Title>
-                    <h2>Precisando de&nbsp;</h2>
-                    <h2 className="destak"> dinheiro?</h2>
-                </S.Title>
-                <S.Subtitle>
-                    <h3 className="subtitle_destak">Fácil&nbsp;</h3>
-                    <h3>e</h3>
-                    <h3 className="subtitle_destak">&nbsp;Rápido</h3>
-                </S.Subtitle>
-
-    
-                <video 
-                    id="vid"
-                    // src="https://firebasestorage.googleapis.com/v0/b/curso-dev-2e4db.appspot.com/o/video%2Fmastercred.mp4?alt=media&token=645b101d-4bf7-4317-a6a8-3537a8094ee2&_gl=1*1qxwmez*_ga*MTkzOTk4MDgyNC4xNjk4NzYwNjQz*_ga_CW55HF8NVT*MTY5ODc2MDY0My4xLjEuMTY5ODc2MDY5Ni43LjAuMA.." 
-                    controls
-                    width={450}
-                    height={600}
-                    poster='https://firebasestorage.googleapis.com/v0/b/curso-dev-2e4db.appspot.com/o/img%2Fjuliana-paes.jpg?alt=media&token=0cd16e86-30fc-46be-9879-5cd551b3722b&_gl=1*tg7exo*_ga*MTkzOTk4MDgyNC4xNjk4NzYwNjQz*_ga_CW55HF8NVT*MTY5ODc2NzY5Ny4yLjEuMTY5ODc2NzczNC4yMy4wLjA.'
-                    src="https://firebasestorage.googleapis.com/v0/b/curso-dev-2e4db.appspot.com/o/video%2Fmastercred.mp4?alt=media&token=645b101d-4bf7-4317-a6a8-3537a8094ee2&_gl=1*1qxwmez*_ga*MTkzOTk4MDgyNC4xNjk4NzYwNjQz*_ga_CW55HF8NVT*MTY5ODc2MDY0My4xLjEuMTY5ODc2MDY5Ni43LjAuMA.."
-                >
-                </video> 
-                                    </S.HeadlineDescription> 
-        </S.HeadlineContainer>
-    )
+export default function Headline() {
+  return (
+    <S.HeadlineContainer>
+      <S.HeadlineDescription>
+        <S.LogoIcon src={Logo} width={180} alt="" />
+        <S.Title>
+          <h2>Precisando de&nbsp;</h2>
+          <h2 className="destak"> dinheiro?</h2>
+        </S.Title>
+        <S.Subtitle>
+          <h3 className="subtitle_destak">Fácil&nbsp;</h3>
+          <h3>e</h3>
+          <h3 className="subtitle_destak">&nbsp;Rápido</h3>
+        </S.Subtitle>
+        <S.Video
+          id="vid"
+          // src="https://firebasestorage.googleapis.com/v0/b/curso-dev-2e4db.appspot.com/o/video%2Fmastercred.mp4?alt=media&token=645b101d-4bf7-4317-a6a8-3537a8094ee2&_gl=1*1qxwmez*_ga*MTkzOTk4MDgyNC4xNjk4NzYwNjQz*_ga_CW55HF8NVT*MTY5ODc2MDY0My4xLjEuMTY5ODc2MDY5Ni43LjAuMA.."
+          controls
+          width={320}
+          height={580}
+          poster="https://firebasestorage.googleapis.com/v0/b/curso-dev-2e4db.appspot.com/o/img%2Fjuliana-paes.jpg?alt=media&token=0cd16e86-30fc-46be-9879-5cd551b3722b&_gl=1*tg7exo*_ga*MTkzOTk4MDgyNC4xNjk4NzYwNjQz*_ga_CW55HF8NVT*MTY5ODc2NzY5Ny4yLjEuMTY5ODc2NzczNC4yMy4wLjA."
+          src="https://firebasestorage.googleapis.com/v0/b/curso-dev-2e4db.appspot.com/o/video%2Fmastercred.mp4?alt=media&token=645b101d-4bf7-4317-a6a8-3537a8094ee2&_gl=1*1qxwmez*_ga*MTkzOTk4MDgyNC4xNjk4NzYwNjQz*_ga_CW55HF8NVT*MTY5ODc2MDY0My4xLjEuMTY5ODc2MDY5Ni43LjAuMA.."
+        ></S.Video>
+        <S.Photo src={Juliana} width={470} alt="MasterCred" />
+      </S.HeadlineDescription>
+    </S.HeadlineContainer>
+  );
 }
--- app/components/headline/headeline.styles.ts
+++ app/components/headline/headeline.styles.ts
@@ -2,51 +2,60 @@ import Image from "next/image";
 import { styled } from "styled-components";
 
 export const HeadlineContainer = styled.section`
-    background-color: #004AAB;
-`
+  background-color: #004aab;
+`;
 
 export const HeadlineDescription = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export const LogoIcon = styled(Image)`
+export const LogoIcon = styled(Image)``;
 
-`
+export const Photo = styled(Image)`
+  position: absolute;
+  top: 30px;
+  z-index: 1;
+  right: 0px;
+`;
 
 export const Title = styled.div`
-    display: flex;
+  display: flex;
 
-    h2 {
-        color: #fff;
-        text-transform: uppercase;
-        letter-spacing: -2px;
-        font-size: 34px;
-    }
+  h2 {
+    color: #fff;
+    text-transform: uppercase;
+    letter-spacing: -2px;
+    font-size: 34px;
+  }
 
-    .destak {
-        background-color: #F48035;
-    }
-`
+  .destak {
+    background-color: #f48035;
+  }
+`;
 
 export const Subtitle = styled.div`
-    display: flex;
-    
-    h3 {
-        color: #fff;
-        text-transform: uppercase;
-        font-size: 59px;
-        font-weight: 700;
-        letter-spacing: -2px;
-        text-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-    }
+  display: flex;
 
-    .subtitle_destak {
-        color: #F48035;
-    }
-`
+  h3 {
+    color: #fff;
+    text-transform: uppercase;
+    font-size: 59px;
+    font-weight: 700;
+    letter-spacing: -2px;
+    text-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  }
 
-export const Juliana = styled(Image)`
-    
-`
+  .subtitle_destak {
+    color: #f48035;
+  }
+`;
+
+export const Juliana = styled(Image)``;
+
+export const Video = styled.video`
+  position: relative;
+  top: 30px;
+  z-index: 2;
+`;
