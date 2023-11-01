29725ffe210dc8e0a4b39f317e5435daaeb8bccc
Oséias Costa <oseiasc2@gmail.com>
2023-11-01 09:50:47 -0300

    feat(Services): create services components

--- app/components/services/Services.tsx
+++ app/components/services/Services.tsx
@@ -1,9 +1,35 @@
-import { ServicesContainer } from "./services.styles";
+"use client";
+import * as S from "./services.styles";
+import Aposentados from '@/public/services/aposentados.png'
+import Militares from '@/public/services/militares.png'
+import Servidores from '@/public/services/servidores.png'
+import Star from '@/public/services/star.svg'
+import Money from '@/public/services/money.svg'
+import Military from '@/public/services/chat.svg'
 
 export default function Services(){
     return(
-        <ServicesContainer>
-
-        </ServicesContainer>
+        <S.ServicesContainer>
+            <h2>oferecemos empréstimos para:</h2>
+            <S.CardsPhotos>
+                <S.ImageServices src={Aposentados} alt='MasterCred - Emprestimo para Aposentados e Pensionistas' />
+                <S.ImageServices src={Militares} alt='MasterCred - Emprestimo para Militares' />
+                <S.ImageServices src={Servidores} alt='MasterCred - Emprestimo para Servidores Públicos' />
+            </S.CardsPhotos>
+            <S.CardsIcons>
+                <S.Card>
+                    <S.IconServices alt='MasterCred - Sem Consulta ao SPC/Serasa' src={Star} />
+                    <h4>Sem Consulta ao SPC/Serasa</h4>
+                </S.Card>
+                <S.Card>
+                    <S.IconServices alt='MasterCred - Dinheiro Rápido e Fácil' src={Money} />
+                    <h4>Dinheiro Rápido e Fácil</h4>
+                </S.Card>
+                <S.Card>
+                    <S.IconServices alt='MasterCred - Atendimento Personalizado' src={Military} />
+                    <h4>Atendimento Personalizado</h4>
+                </S.Card>
+            </S.CardsIcons>
+        </S.ServicesContainer>
     )
 }
--- app/components/services/services.styles.ts
+++ app/components/services/services.styles.ts
@@ -1,5 +1,72 @@
+import Image from "next/image";
 import { styled } from "styled-components";
 
 export const ServicesContainer = styled.section`
+    padding-top: 63px;
+    padding-bottom: 63px;    
+
+    h2 {
+        color: #002D68;
+        text-transform: uppercase;
+        text-align: center;
+        font-weight: 900;
+        font-size: 36px;
+
+        @media(max-width: 740px){
+            font-size: 30px;
+        }
+    }
+`
+
+export const CardsPhotos = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    @media(max-width: 740px) {
+        flex-direction: column;
+        align-items: center;
+    }
     
+`
+
+export const CardsIcons = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    @media(max-width: 740px) {
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+export const ImageServices = styled(Image)`
+    background-color: #fff;
+    width: 220px;
+    height: auto;
+    margin: 20px;
+`
+
+export const IconServices = styled(Image)`
+    width: 54px;
+    height: auto;
+    background-color: #002D68;
+    border-radius: 12px 0px 0px 12px;
+`
+
+export const Card = styled.div`
+    background-color: #002D68;
+    width: 220px;
+    display: flex;
+    margin: 20px;
+    border-radius: 10px;
+    align-items: center;
+    padding: 4px;
+
+    h4 {
+        color: #fff;
+        text-transform: uppercase;
+        padding-left: 7px;
+    }
 `
